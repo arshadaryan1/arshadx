96758a8309181cd05e35ea789979c428d081af51
arshadaryan1 <96657846+arshadaryan1@users.noreply.github.com>
2023-04-05 13:39:16 +0530

    aa

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("main-div").style.display = "block";
 }
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
